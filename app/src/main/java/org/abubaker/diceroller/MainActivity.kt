db69e7077d35997123e825b7a82ad69e60788b32
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-14 16:34:20 +0500

    Exercise completed.

--- app/src/main/java/org/abubaker/diceroller/MainActivity.kt
+++ app/src/main/java/org/abubaker/diceroller/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import org.abubaker.diceroller.databinding.ActivityMainBinding
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
@@ -16,14 +20,33 @@ class MainActivity : AppCompatActivity() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(mBinding.root)
 
-        mBinding.btnRoll.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-
-            mBinding.resultTextView.text = "6"
-        }
+        mBinding.btnRoll.setOnClickListener { rollDice() }
 
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        mBinding.resultTextView.text = diceRoll.toString()
+
+        // Sample toast message
+        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
 
 }
