375862d9db9997bad024bf19bdb8b6af500f5f7a
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-15 01:22:19 +0500

    Completed basics of conducting Unit Tests

--- app/src/test/java/org/abubaker/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/org/abubaker/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package org.abubaker.diceroller
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +10,33 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Test
+    fun generates_number(){
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(5, 2 + 3)
     }
+
+    @Test
+    fun subtraction_isCorrect() {
+        assertEquals(7, 9 - 2)
+    }
+
 }
+
+/**
+ * Note: There are many assertions in the JUnit library. Some common assertions you might encounter are:
+ * 1. assertEquals()
+ * 2. assertNotEquals()
+ * 3. assertThat()
+ * 4. assertTrue()
+ * 5. assertFalse()
+ * 6. assertNull()
+ * 7. assertNotNull()
+ */
